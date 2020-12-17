2b00f855635ce39a15efd98ef2e0789ae613e6c2
ksk2345 <73072516+ksk2345@users.noreply.github.com>
2020-12-18 01:22:35 +0530

    minor updates

--- gnosis-ctf/migrations/3_deploy_contract_ex.js
+++ gnosis-ctf/migrations/3_deploy_contract_ex.js
@@ -2,5 +2,5 @@ var RewardsEngine = artifacts.require("./RewardsEngine.sol");
 
 module.exports = function(deployer) {
   //deployer.deploy(RewardsEngine, rToken.address, cTokens.address);
-  deployer.deploy(RewardsEngine, "0xeA41aA8c30fcf7f29BdCE6E0eCd97248F17bd2b9", "0xCBef9084A9491c0874b8660F20a2e4b1633ED83E");
+  deployer.deploy(RewardsEngine, "0x2850eA2599E5d68e53aC3d6B3fe1A67B7cd657Ad", "0x2Fd724d4C7A14f86e0002257d6009d2a17678a3C");
 };
